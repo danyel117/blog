4e0fedbe36c8db352931e723c7eaef86f43c6a35
danyel117 <dsaldarriaga@prevalentware.com>
2020-07-30 09:02:50 -0500

    agregar iconos en header

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-
+import '../assets/font/flaticon.css'
 const Landing = () => {
     return ( 
         <header>
             <section className="header-icons-container">
                 <div className="icons">
-                    <a href="/"><span></span></a>
-                    <a href="/"><span></span></a>
-                    <a href="/"><span></span></a>
-                    <a href="/"><span></span></a>
-                    <a href="/"><span></span></a>
+                    <a href="/"><span className="flaticon-001-facebook"></span></a>
+                    <a href="/"><span className="flaticon-002-twitter"></span></a>
+                    <a href="/"><span className="flaticon-011-instagram"></span></a>
+                    <a href="/"><span className="flaticon-010-linkedin"></span></a>
+                    <a href="/"><span className="flaticon-008-youtube"></span></a>
                 </div>
             </section>
             <nav>
