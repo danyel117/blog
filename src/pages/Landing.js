de19038ed0a6bc1f0e6330c06c0de32278500147
danyel117 <dsaldarriaga@prevalentware.com>
2020-07-30 10:22:16 -0500

    agregar fuente roboto e imagen de background

---
+++ src/pages/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Landing = () => {
+    return ( 
+        <main className="home-main">
+            <section>
+                <p className="home-main-text">Conoce las novedades y noticias del mundo tech</p>
+                <button>
+                    <a href="/">entra ya</a>
+                </button>
+            </section>
+        </main>
+     );
+}
+ 
+export default Landing
